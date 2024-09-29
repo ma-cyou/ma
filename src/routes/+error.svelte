9a2ddf325c0d5a5b83fd0cbbd97b8509b744b15d
Mapagmataas <dev.ma.cyou@gmail.com>
2024-09-30 01:39:46 +0700

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,11 +5,12 @@
 	let errorMessage: string;
 
 	$: errorStatus = $page.status || 500;
-	$: errorMessage = $page.error?.message || 'An unexpected error occurred.';
+	$: errorMessage = $page.error?.body?.message || $page.error?.message ||  'An unexpected error occurred.';
 </script>
 
 <div class="error-page">
 	<h1>{errorStatus}</h1>
 	<p>{errorMessage}</p>
+	<button on:click={() => history.back()}>Go Back</button>
 	<a href="/">Go back to the homepage</a>
 </div>
