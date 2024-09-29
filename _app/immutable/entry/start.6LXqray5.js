037ec99a5f0f8f763b7d917ea560a0130f52f859
Mapagmataas <dev.ma.cyou@gmail.com>
2024-09-30 01:39:42 +0700

    Updates

---
+++ _app/immutable/entry/start.6LXqray5.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.o5GH5M84.js";export{t as start};
--- _app/immutable/nodes/2.WKLUxNEm.js
+++
@@ -1,1 +0,0 @@
-import{a,t}from"../chunks/disclose-version.DJrvxNq7.js";var p=t("<h1>Welcome to the homepage!</h1>");function r(e){var o=p();a(e,o)}export{r as component};
