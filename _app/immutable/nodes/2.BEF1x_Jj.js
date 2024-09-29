c7debcd1328dcc11fce5d17fcc0e4ee37eaf84c2
Mapagmataas <dev.ma.cyou@gmail.com>
2024-09-29 23:18:47 +0700

    Updates

--- _app/immutable/nodes/2.BEF1x_Jj.js
+++
@@ -1,1 +0,0 @@
-import{b as t,t as a}from"../chunks/disclose-version.B3M4yZ7B.js";var p=a("<h1>Welcome to the homepage!</h1>");function r(e){var o=p();t(e,o)}export{r as component};
---
+++ _app/immutable/nodes/2.Cnu4orOm.js
@@ -0,0 +1,1 @@
+import{a,t}from"../chunks/disclose-version.DJk0yjrV.js";var p=t("<h1>Welcome to the homepage!</h1>");function r(e){var o=p();a(e,o)}export{r as component};
