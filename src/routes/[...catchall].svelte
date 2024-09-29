3233d85929de14a44360a266c96894f73398e647
Mapagmataas <dev.ma.cyou@gmail.com>
2024-09-29 22:06:56 +0700

    update

--- src/routes/[...catchall].svelte
+++
@@ -1,3 +0,0 @@
-<!-- src/routes/[...catchall].svelte -->
-<h1>404 - Page Not Found</h1>
-<p>The page you are looking for does not exist.</p>
---
+++ src/routes/[username]/+page.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+    const usernames = ['john', 'jane', 'alice'];
+
+    import { error } from '@sveltejs/kit';
+    
+    export let params
+    
+    const username = params.username;
+
+    if (!usernames.includes(username)) {
+        throw error(404, `User ${username} not found`);
+    }
+</script>
+
+<h1>Hello {params.username}!</h1>
