87d99ce8f77ee8352e949e22ef779e4ad79a2d1d
Mapagmataas <dev.ma.cyou@gmail.com>
2024-10-13 00:31:41 +0700

    Update Head.svelte

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -3,7 +3,6 @@
 	export let title: string = 'macyou';
 	export let description: string = "Mapagmataas' website";
 	export let keywords: string = 'ma, cyou, macyou, mapagmataas, ma-cyou, ma.cyou';
-	export let author: string = 'Mapagmataas';
 	export let ifOg: boolean = true;
 	export let url: string = 'https://ma.cyou';
 
@@ -71,29 +70,30 @@
 	<title>{title}</title>
 	<meta name="description" content={description} />
 	<meta name="keywords" content={keywords} />
-	<meta name="author" content={author} />
+	<meta name="author" content="Mapagmataas" />
 	<meta name="robots" content="index, follow" />
 
 	<link rel="canonical" href={url} />
 
-	{#if ifOg}
+	{#if ifOg !== null}
 		<!-- OpenGraph Meta Tags -->
 		<meta property="og:title" content={title} />
 		<meta property="og:description" content={description} />
-		<meta
-			property="og:image"
-			content="https://dynamic-og-image-generator.vercel.app/api/generate?title={description}&author={title}&websiteUrl={url}&avatar=https%3A%2F%2Ftest.ma.cyou%2Fandroid-chrome-512x512-light.png&theme=default"
-		/>
 		<meta property="og:url" content={url} />
 		<meta property="og:type" content="website" />
 		<meta property="og:site_name" content={title} />
 		<meta property="og:image:alt" content="OpenGraph Image" />
+		<meta
+			property="og:image"
+			content="https://dynamic-og-image-generator.vercel.app/api/generate?title={description}&author={title}&websiteUrl={url}&avatar=https%3A%2F%2Ftest.ma.cyou%2Fandroid-chrome-512x512-light.png&theme=default"
+		/>
 
 		<!-- Twitter Meta Tags -->
 		<meta name="twitter:card" content="summary_large_image" />
-		<meta name="twitter:site" content={'@' + author} />
+		<meta name="twitter:url" content={url} />
 		<meta name="twitter:title" content={title} />
 		<meta name="twitter:description" content={description} />
+		<meta name="twitter:alt" content="Twitter Image" />
 		<meta
 			name="twitter:image"
 			content="https://dynamic-og-image-generator.vercel.app/api/generate?title={description}&author={title}&websiteUrl={url}&avatar=https%3A%2F%2Ftest.ma.cyou%2Fandroid-chrome-512x512-light.png&theme=default"
