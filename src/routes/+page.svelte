8cfa7c1523ded94f3c403312f305049151e69b36
Mapagmataas <dev.ma.cyou@gmail.com>
2024-10-13 02:13:45 +0700

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { language } from '$lib/stores/language';
 </script>
 
-<div class="flex h-full w-full flex-col items-center justify-center">
+<div class="flex h-full w-full flex-col items-center justify-center px-4 py-16">
 	<h1 class="text-4xl font-bold">
 		{$language === 'ru' ? 'Добро пожаловать на Главную Страницу!' : 'Welcome to the Home Page!'}
 	</h1>
