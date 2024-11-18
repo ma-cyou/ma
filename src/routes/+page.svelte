c74d1d4bcfec4e323a5e516b4914da812d98ae61
Mapagmataas <154022631+ma-cyou@users.noreply.github.com>
2024-11-18 12:06:32 +0700

    Replacing shared files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,31 +7,6 @@
 </script>
 
 <div
-	class="fixed bottom-4 right-4 flex w-full max-w-sm flex-col-reverse -space-y-12 space-y-reverse transition-all duration-300 ease-in-out hover:space-y-1 hover:space-y-reverse"
->
-	<Alert.Root>
-		<Rocket class="size-4" />
-		<Alert.Title>Default Alert 1</Alert.Title>
-		<Alert.Description>You can add components to your app using the cli.</Alert.Description>
-	</Alert.Root>
-	<Alert.Root>
-		<Rocket class="size-4" />
-		<Alert.Title>Default Alert 2</Alert.Title>
-		<Alert.Description>You can add components to your app using the cli.</Alert.Description>
-	</Alert.Root>
-	<Alert.Root variant="destructive" class="bg-background">
-		<ExclamationTriangle class="size-4" />
-		<Alert.Title>Error Altert</Alert.Title>
-		<Alert.Description>Your session has expired. Please login again.</Alert.Description>
-	</Alert.Root>
-	<Alert.Root>
-		<Rocket class="size-4" />
-		<Alert.Title>Default Alert 3</Alert.Title>
-		<Alert.Description>You can add components to your app using the cli.</Alert.Description>
-	</Alert.Root>
-</div>
-
-<div
 	class="mx-auto flex min-h-[calc(100dvh_-_3.5rem)] w-full flex-col items-center justify-center bg-gradient-to-br from-white to-sky-200 px-1 dark:from-sky-800 dark:to-black md:min-h-[calc(100dvh_-_4rem)]"
 >
 	<Button variant="outline" href="https://ma.cyou" class="mb-4"
